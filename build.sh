8720dbceab146de84c34557c69f0436e36029294
r1ddl3m37h15 <r1ddl3m37h15@users.noreply.github.com>
2021-04-01 21:29:59 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,4 @@ hostname
 whoami
 pwd
 
-#docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp hammady/centos-jdk8-ant:latest ant
 docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp jdk8-ant:latest ant $*
