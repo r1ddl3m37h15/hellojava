6ad84b6e1375e686ac6f8be7c4dbaefd536bd1f8
Jeff Suess <j7057@erieinsurance.com>
2021-05-06 15:37:17 -0400

    demo

--- build.bat
+++ build.bat
@@ -7,5 +7,5 @@
 set CI_JOB_ID=0001
 set CI_PIPELINE_URL=localhost
 
-docker run --rm -v "%CD%":/usr/src/myapp -w /usr/src/myapp -e CI_JOB_ID:%CI_JOB_ID% -e CI_PIPELINE_URL:%CI_PIPELINE_URL% r1ddl3m37h15/jdk8-ant:v2 ant %*
+docker run --rm -v "%CD%":/usr/src/myapp -w /usr/src/myapp --env CI_JOB_ID=%CI_JOB_ID% --env CI_PIPELINE_URL=%CI_PIPELINE_URL% r1ddl3m37h15/jdk8-ant:v2 ant %*
 if ERRORLEVEL 1 exit /b 1
