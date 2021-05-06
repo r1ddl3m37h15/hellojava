6aa494a107fc9b592801c95f8495c7cced6eabfe
Jeff Suess <j7057@erieinsurance.com>
2021-05-06 15:15:11 -0400

    updates for demo

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
-@echo off
+@echo on
 
 @rem
 @rem building on local
 @rem
 
 set LOG_URL=localhost
-set JOB_NUMBER=00001
+set JOB_NUMBER=0001
 
-docker run --rm -v "%CD%":/usr/src/myapp -w /usr/src/myapp r1ddl3m37h15/jdk8-ant:v2 ant %*
+docker run --rm -v "%CD%":/usr/src/myapp -w /usr/src/myapp -e JOB_NUMBER:%JOB_NUMBER% -e LOG_URL:%LOG_URL% r1ddl3m37h15/jdk8-ant:v2 ant %*
 if ERRORLEVEL 1 exit /b 1
